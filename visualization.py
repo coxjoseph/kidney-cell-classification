e3b19f84bdf863c1d9f057c45b07b0e5c57e1c5c
Foundedd <founded@founded.gay>
2024-03-04 17:01:58 -0500

    initial visualization script

---
+++ visualization.py
@@ -0,0 +1,45 @@
+from os import PathLike
+from typing import Union
+import numpy as np
+import matplotlib.pyplot as plt
+import tifffile
+
+
+def load_tiff_as_array(file_path: Union[str, PathLike]) -> np.ndarray:
+    """
+    Load a TIFF image and convert it to a numpy array.
+
+    Parameters:
+    - file_path: Path to the TIFF file.
+
+    Returns:
+    - A numpy array representing the image.
+    """
+    with tifffile.TiffFile(file_path) as tiff:
+        image_array = tiff.asarray()
+    return image_array
+
+
+def display_channel_heatmaps(image_array: np.ndarray, channel_indices: list):
+    """
+    Display specified channels of the image as heatmaps.
+
+    Parameters:
+    - image_array: A numpy array representing the image.
+    - channel_indices: A list of indices of the channels to be displayed.
+    """
+    for index in channel_indices:
+        plt.figure(figsize=(10, 8))
+        plt.imshow(image_array[:, :, index], cmap='hot')
+        plt.title(f'Channel {index} Heatmap')
+        plt.colorbar()
+        plt.show()
+
+
+if __name__ == '__main__':
+    file_path = 'data/codex.tif'
+    image_array = load_tiff_as_array(file_path)
+
+    print(f'Image array is {"x".join(map(str, image_array.shape))}')
+    channel_indices = [0, 1, 2]
+    display_channel_heatmaps(image_array, channel_indices)
