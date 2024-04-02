86a2d6c581d017568fd48c727e3b4a120ab7c462
dylanogrodowski <OgrodowskiDylan@gmail.com>
2024-04-02 04:35:38 -0400

    Added cell radii approximator

--- classify.py
+++ classify.py
@@ -2,7 +2,8 @@ import argparse
 import logging
 import numpy as np
 from images import load_images, generate_classified_image
-from cells import segment_nuclei_brightfield, segment_nuclei_dapi, calculate_radii_from_nuclei, create_cells, extract_nuclei_coordinates
+from cells import segment_nuclei_brightfield, segment_nuclei_dapi, calculate_radii_from_nuclei, create_cells, extract_nuclei_coordinates, create_nuclei, get_nucleus_mask
+from visualization import show_codex_window
 from skimage import io, transform
 from feature_extraction import generate_feature_extractors
 from clustering import cluster
@@ -36,39 +37,27 @@ if __name__ == '__main__':
     args = parse_arguments()
 
     brightfield, codex = load_images(args)
+    DAPI_index = 0 # Remove once arg parser is done
     
     # START OF TESTING CODE
-    #nuclei = [(5906, 7187)] # Hardcoded nuclei value until nuclei extractor code is done
-    #nuclei =[(4062, 3086)]
-    #radii = [15]; # Arbitrary for now
-    #cells = create_cells(nuclei, radii)
-    #nuclei_mask = cells[0].get_cell_mask_irregular(codex, DAPI_index=0, cyto_index=1, visual_output=True) # Cyto index is a placeholder, not currently used
-    
-    use_debug_coordinates = True
-    if (not use_debug_coordinates):
-        print('Beginning nuclei mask generation...', flush=True)
-        nuclei_mask = segment_nuclei_dapi(codex, DAPI_index=0, visual_output=False)
-        nuclei_list = extract_nuclei_coordinates(nuclei_mask, downsample_factor=4, num_processes=8, visual_output=False)
-        nuclei_subsample = [nuclei_list[10000], nuclei_list[20000], nuclei_list[30000]]# Grab a small randomish sample of nuclei for testing
-        radii = [1, 1, 1] # Temporary values for debugging
-        cells = create_cells([nuclei_subsample[0].center, nuclei_subsample[1].center, nuclei_subsample[2].center], radii)
-        nuclei_mask = nuclei_mask = cells[0].get_cell_mask_irregular(codex, DAPI_index=0, cyto_index=1, visual_output=True) # Cyto index is a placeholder, not currently used
-        nuclei_mask = nuclei_mask = cells[1].get_cell_mask_irregular(codex, DAPI_index=0, cyto_index=1, visual_output=True) # Cyto index is a placeholder, not currently used
-        nuclei_mask = nuclei_mask = cells[2].get_cell_mask_irregular(codex, DAPI_index=0, cyto_index=1, visual_output=True) # Cyto index is a placeholder, not currently used
-    else:
-        nuclei_subsample = [(1718,5018),(1986,1410),(4062,3084)] # These coordinates were grabbed with a ds factor=4
-        radii = [1, 1, 1] # Temporary values for debugging
-        cells = create_cells(nuclei_subsample, radii)
-        nuclei_mask = nuclei_mask = cells[0].get_cell_mask_irregular(codex, DAPI_index=0, cyto_index=1, visual_output=True) # Cyto index is a placeholder, not currently used
-        nuclei_mask = nuclei_mask = cells[1].get_cell_mask_irregular(codex, DAPI_index=0, cyto_index=1, visual_output=True) # Cyto index is a placeholder, not currently used
-        nuclei_mask = nuclei_mask = cells[2].get_cell_mask_irregular(codex, DAPI_index=0, cyto_index=1, visual_output=True) # Cyto index is a placeholder, not currently used
-    
+    #use_debug_coordinates = True
+    #if (not use_debug_coordinates):
+    #    print('Beginning nuclei mask generation...', flush=True)
+    #    nuclei_mask = segment_nuclei_dapi(codex, DAPI_index=0, visual_output=False)
+    #    nuclei_list = extract_nuclei_coordinates(nuclei_mask, downsample_factor=4, num_processes=8, visual_output=False)
+    #    nuclei_subsample = [nuclei_list[10000], nuclei_list[20000], nuclei_list[30000]]# Grab a small randomish sample of nuclei for testing
+    #    nuclei_mask = get_nucleus_mask(nuclei_subsample[2], codex, DAPI_index=0, visual_output=True)
+    #else:
+    #    nuclei_subsample = [(1718,5018),(1986,1410),(4062,3084)] # These coordinates were grabbed in a prior run
+    #    nuclei_mask = get_nucleus_mask(nuclei_subsample[2], codex, DAPI_index=0, visual_output=True)
+    #    for codex_index in range(0, 40):
+    #        show_codex_window(cells[2], codex_index, codex)
     # END OF TESTING CODE
     
-    # Temporarily commented out for testing
-    #nuclei = segment_nuclei_brightfield(brightfield)
-    #radii = calculate_radii_from_nuclei(nuclei)
-    #cells = create_cells(nuclei, radii)
+    nuclei_mask = segment_nuclei_dapi(codex, DAPI_index=DAPI_index)
+    nuclei = extract_nuclei_coordinates(nuclei_mask, downsample_factor=4, num_processes=8, visual_output=False)
+    radii = calculate_radii_from_nuclei(nuclei, codex, DAPI_index=DAPI_index)
+    cells = create_cells(nuclei, radii)
 
     #feature_extractors = generate_feature_extractors()
     #[cell.calculate_features(feature_extractors, codex) for cell in cells]
